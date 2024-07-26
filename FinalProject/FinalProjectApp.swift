875766a9d2856e049fd2c2b081633026b5f32a8d
Sadhika <sadhikac357@gmail.com>
2024-07-26 11:59:54 -0500

    Initial Commit

---
+++ FinalProject/FinalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalProjectApp.swift
+//  FinalProject
+//
+//  Created by SriSai on 7/26/24.
+//
+
+import SwiftUI
+
+@main
+struct FinalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
