437e2eb597f064291f97428381a5f5bd4c2be74c
Nadia Begum <76848926+Nadia-Begum@users.noreply.github.com>
2024-07-30 22:21:53 +0100

    Added images to the navigation tab on ContentView. Completed the functionality of the HomeScreen.

---
+++ FinalProject/Journal.swift
@@ -0,0 +1,18 @@
+//
+//  Journal.swift
+//  FinalProject
+//
+//  Created by Nadia Begum on 30/07/2024.
+//
+
+import SwiftUI
+
+struct Journal: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Journal()
+}
---
+++ FinalProject/Tasks.swift
@@ -0,0 +1,18 @@
+//
+//  Tasks.swift
+//  FinalProject
+//
+//  Created by Nadia Begum on 30/07/2024.
+//
+
+import SwiftUI
+
+struct Tasks: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Tasks()
+}
